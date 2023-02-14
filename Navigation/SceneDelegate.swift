8bb80e34d1229d17c040ee6ba0b95a661cc29a53
Amsterdance26 <amsterdance26@gmail.com>
2023-02-14 14:03:01 +0700

    Измените Tab Bar Item у добавленных контроллеров, добавьте заголовок и картинку. Картинки можно добавить в Assets.xcassets или использовать SF Symbols.
    Измените Tab Bar Item у добавленных контроллеров, добавьте заголовок и картинку. Картинки можно добавить в Assets.xcassets или использовать SF Symbols.

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -8,17 +8,28 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let feedViewController = UIViewController()
+        let profileViewController = UIViewController()
+
+        // Set up navigation controllers with view controllers
+        let feedNavigationController = UINavigationController(rootViewController: feedViewController)
+        feedNavigationController.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
+        profileNavigationController.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
+
+        // Set up tab bar controller with navigation controllers
         let tabBarController = UITabBarController()
-        let feedNavController = UINavigationController(rootViewController: FeedViewController())
-        let profileNavController = UINavigationController(rootViewController: ProfileViewController())
-        tabBarController.setViewControllers([feedNavController, profileNavController], animated: false)
+        tabBarController.viewControllers = [feedNavigationController, profileNavigationController]
+
+        // Set up window
         let window = UIWindow(windowScene: windowScene)
         window.rootViewController = tabBarController
         self.window = window
         window.makeKeyAndVisible()
     }
+}
