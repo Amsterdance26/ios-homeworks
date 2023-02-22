5b00131d67c43a4ef3e8b44f377098b3b8bb3cc1
Amsterdance26 <amsterdance26@gmail.com>
2023-02-23 01:19:52 +0700

    Waiting for something...

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ProfileHeaderView: UIView {
     let avatarImageView = UIImageView()
+    let nameLabel = UILabel()
+    let statusLabel = UILabel()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -17,12 +19,31 @@ class ProfileHeaderView: UIView {
         let avatarImage = UIImage(named: "Cat")
         avatarImageView.image = avatarImage
         avatarImageView.contentMode = .scaleAspectFill
+        avatarImageView.layer.cornerRadius = 30
         avatarImageView.layer.masksToBounds = true
         avatarImageView.layer.borderWidth = 3
         avatarImageView.layer.borderColor = UIColor.white.cgColor
 
         // Add avatar image view as subview
         addSubview(avatarImageView)
+
+        // Set up name label
+        nameLabel.text = "White Cat"
+        nameLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        nameLabel.textColor = .white
+
+        // Add name label as subview
+        addSubview(nameLabel)
+
+        // Set up status label
+        statusLabel.text = "Waiting for something..."
+        statusLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        statusLabel.textColor = .gray
+
+        // Add status label as subview
+        addSubview(statusLabel)
+
+        
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -36,11 +57,22 @@ class ProfileHeaderView: UIView {
         let safeArea = self.safeAreaInsets
         let inset: CGFloat = 16
         let borderWidth: CGFloat = 3
-        let avatarSize = min(bounds.width - safeArea.left - safeArea.right * inset, bounds.height - safeArea.top - safeArea.bottom * inset, 2000)
+        let avatarSize: CGFloat = 60
         let avatarOrigin = CGPoint(x: safeArea.left + inset, y: safeArea.top + inset)
-        avatarImageView.layer.cornerRadius = avatarSize / 2
-
         avatarImageView.frame = CGRect(origin: avatarOrigin, size: CGSize(width: avatarSize, height: avatarSize))
         avatarImageView.layer.borderWidth = borderWidth
+
+        // Set frame of name label
+        let nameLabelOrigin = CGPoint(x: avatarImageView.frame.maxX + inset, y: safeArea.top + 27)
+        let nameLabelSize = nameLabel.sizeThatFits(CGSize(width: bounds.width - nameLabelOrigin.x - inset, height: bounds.height - nameLabelOrigin.y - inset))
+        nameLabel.frame = CGRect(origin: nameLabelOrigin, size: nameLabelSize)
+
+        // Set frame of status label
+        let statusLabelOrigin = CGPoint(x: nameLabelOrigin.x, y: nameLabel.frame.maxY + 8)
+        let statusLabelSize = statusLabel.sizeThatFits(CGSize(width: bounds.width - statusLabelOrigin.x - inset, height: bounds.height - statusLabelOrigin.y - inset))
+        statusLabel.frame = CGRect(origin: statusLabelOrigin, size: statusLabelSize)
+
+        
+
     }
 }
