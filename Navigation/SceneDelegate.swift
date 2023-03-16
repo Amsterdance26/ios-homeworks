41e940e6703be1b13bb945571abaae0ea50127a4
Amsterdance26 <amsterdance26@gmail.com>
2023-03-16 17:58:13 +0700

    Add LogInViewController

--- Navigation/SceneDelegate.swift
+++ Navigation/SceneDelegate.swift
@@ -16,13 +16,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let feedViewController = FeedViewController()
-        let profileViewController = ProfileViewController()
         let postViewController = PostViewController()
+        let logInViewController = LogInViewController()
 
         let feedNavigationController = UINavigationController(rootViewController: feedViewController)
         feedNavigationController.tabBarItem = UITabBarItem(title: "Лента", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
-        let profileNavigationController = UINavigationController(rootViewController: profileViewController)
-        profileNavigationController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
+        let logInNavigationController = UINavigationController(rootViewController: logInViewController)
+        logInNavigationController.tabBarItem = UITabBarItem(title: "Профиль", image: UIImage(systemName: "person"), selectedImage: UIImage(systemName: "person.fill"))
         let postNavigationController = UINavigationController(rootViewController: postViewController)
         postNavigationController.tabBarItem = UITabBarItem(title: "Пост", image: UIImage(systemName: "message.fill"), selectedImage: UIImage(systemName: "message.fill"))
 
@@ -30,7 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         postViewController.post = myPost
 
         let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [feedNavigationController, profileNavigationController, postNavigationController]
+        tabBarController.viewControllers = [feedNavigationController, logInNavigationController, postNavigationController]
         tabBarController.tabBar.backgroundColor = UIColor.white
 
         let window = UIWindow(windowScene: windowScene)
